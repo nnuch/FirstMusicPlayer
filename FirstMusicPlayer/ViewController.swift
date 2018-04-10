1249e084aa9dc3237755a062f81181df8146a8e7
nnuch <Noochie@nnsmac.local>
2018-04-09 21:57:50 -0400

    Initial Commit

---
+++ FirstMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstMusicPlayer
+//
+//  Created by Nuch Phromsorn on 2018-04-09.
+//  Copyright © 2018 Nuch Phromsorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

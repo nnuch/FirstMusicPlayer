17b24ba8d0a1a5d49d3ebbf8bc90381fa7010fde
nnuch <Noochie@nnsmac.local>
2018-04-10 21:35:57 -0400

    wrap up storyboard, add colours to all screens

--- FirstMusicPlayer/ViewControl/DetailPlayerViewController.swift
+++ FirstMusicPlayer/ViewControl/DetailPlayerViewController.swift
@@ -9,28 +9,25 @@
 import UIKit
 import AVFoundation
 
-
-
 class DetailPlayerViewController: UIViewController {
    
     @IBOutlet var coverImageView: UIImageView!
     @IBOutlet var songTitleLabel: UILabel!
     @IBOutlet var artistLabel: UILabel!
     
-  
-    @IBOutlet var progressView: UIProgressView!
-    
+    //SliderView - updateprogressView
     @IBOutlet var sliderView: UISlider!
     var smartWidth:CFloat = 0.0
-    
     @IBOutlet var maxTimer: UILabel!
     @IBOutlet var currentTimer: UILabel!
+    
     
     var trackID: Int = 0
     var library = MusicLibrary().library
     var audioPlayer: AVAudioPlayer!
+
     
-    //switch icon-image
+    //Switch toolIcon play button and pause button
     @IBOutlet var toolIcon: RoundButton!
     var tool:UIImageView!
     var isPlaying = true
@@ -46,79 +43,60 @@ class DetailPlayerViewController: UIViewController {
         self.view.addSubview(tool)
         
         
-       // self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.largeTitleDisplayMode = .never
-      // self.view.layer.backgroundColor = UIColor.black.cgColor
         basePlayer()
         
     }
     
 
-    
+    //MARK:  Stop audioPlayer
     override func viewWillDisappear(_ animated: Bool) {
         audioPlayer.stop()
     }
+
     
     
     func basePlayer(){
-        
+    
         if let coverImage = library[trackID]["coverImage"] {
             coverImageView.image = UIImage(named: "\(coverImage).jpg")
-        } else {
-            
         }
+        
         artistLabel.text = library[trackID]["artist"]
         songTitleLabel.text = library[trackID]["title"]
         
         let path = Bundle.main.path(forResource: "\(trackID)", ofType: "mp3")
-        
-        if let path = path {
-            let mp3URL = URL(fileURLWithPath: path)
-        
+        let mp3URL = URL(fileURLWithPath: path!)
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: mp3URL)
                 audioPlayer.currentTime = 0
                 isPlaying = true
                 playAndPause()
                 Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(DetailPlayerViewController.updateProgressView), userInfo: nil, repeats: true)
-               progressView.setProgress(Float(audioPlayer.currentTime/audioPlayer.duration),animated: true)
 
-            } catch let error as NSError {
-                print(error.localizedDescription)
+                } catch let error as NSError {
+                    print(error.localizedDescription)
             }
-        }
-        
     }
     
     
 
     @objc func updateProgressView(){
-//        if audioPlayer.isPlaying {
-//            progressView.setProgress(Float(audioPlayer.currentTime/audioPlayer.duration), animated: true)
-//        }
-//
-        
         if audioPlayer.isPlaying {
-            
             
             sliderView.value = CFloat(audioPlayer.currentTime) // based on ur case
             sliderView.maximumValue = CFloat(audioPlayer.duration)
-            
-            
+
             smartWidth = CFloat(365 * audioPlayer.duration ) / 100
             
             currentTimer.text = String(format: "%.2f",sliderView.value / 60)
-            maxTimer.text = String(format: "%.2f", sliderView.maximumValue / 60)
+            maxTimer.text = String(format: "%.2f","-",sliderView.maximumValue / 60)
             
-//            shadowSlider.frame = CGRectMake( shadowSlider.frame.origin.x , shadowSlider.frame.origin.y , smartWidth , shadowSlider.frame.size.height);
-            
-            
-        
         }
         
     }
 
-    
+    //MARK: Play and Pause buttons
     func playAndPause() {
         if (isPlaying) {
         
@@ -132,11 +110,9 @@ class DetailPlayerViewController: UIViewController {
             toolIcon.setImage(#imageLiteral(resourceName: "play"), for: .normal)
             if audioPlayer.isPlaying {
                 audioPlayer.pause()
+            }
         }
-    
-    }
-    isPlaying = !isPlaying  //revert between true and false for the button icons
-    
+        isPlaying = !isPlaying  //revert between true and false for the button icons
     }
     
     @IBAction func playClicked(_ sender: Any) {
@@ -145,21 +121,25 @@ class DetailPlayerViewController: UIViewController {
     
     
 
-    
+   
+    //MARK:  Previous button
     @IBAction func previousClicked(_ sender: Any) {
         if trackID != 0 || trackID > library.count {
             trackID -= 1
         }
         audioPlayer.currentTime = 0
-        progressView.progress = 0
+        audioPlayer.stop()
         basePlayer()
         
     }
     
     
+    //MARK: Shuffle button
     @IBAction func shuffleClicked(_ sender: Any) {
+        
     }
     
+    //MARK: Next button
     @IBAction func nextClicked(_ sender: Any) {
          trackID += 1
         if trackID > library.count - 1 {
@@ -167,16 +147,9 @@ class DetailPlayerViewController: UIViewController {
           
         }
         audioPlayer.currentTime = 0
-        progressView.progress = 0
+        audioPlayer.stop()
         basePlayer()
     }
     
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
 
 }
--- FirstMusicPlayer/model/MusicLibrary.swift
+++ FirstMusicPlayer/model/MusicLibrary.swift
@@ -10,8 +10,8 @@ import Foundation
 struct MusicLibrary {
     var library = [
         ["title": "Holly Jolly Christmas", "artist": "Michael Buble", "index": "0" , "coverImage":"0"],
-        ["title": "1 name song", "artist": "unknown 1", "index": "1" , "coverImage":"1"],
-        ["title": "2 name song", "artist": "unknown 2", "index": "2" , "coverImage":"2"],
-        ["title": "3 name song", "artist": "unknown 3", "index": "3" , "coverImage":"3"]
+        ["title": "Jazz Piano Christmas", "artist": "Beegie Adair", "index": "1" , "coverImage":"1"],
+        ["title": "Desaree", "artist": "Unknown", "index": "2" , "coverImage":"2"],
+        ["title": "Relaxation Christmas Melody", "artist": "Christmas Melody", "index": "3" , "coverImage":"3"]
     ]
 }
